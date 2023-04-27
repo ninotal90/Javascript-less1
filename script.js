c3d93a1c2f3eaa1605366b3dff81afb38aff3ca1
ninotal90 <ntalakvadze90@gmail.com>
2023-04-27 19:03:25 +0400

    Added HTML and Javascript

---
+++ script.js
@@ -0,0 +1,29 @@
+// if statement
+
+let number = 100;
+
+if (number < 50) {
+  console.log("<50");
+} else if (number > 20) {
+  console.log(">20");
+} else {
+  console.log("error");
+}
+
+let userName = "mariam";
+
+if ((userName = "mariam")) {
+  console.log("true");
+} else {
+  console.log(false);
+}
+
+// swich statement
+
+switch (userName) {
+  case userName = "mariam":
+    console.log("true");
+    break;
+    default:
+        console.log(false)
+}
